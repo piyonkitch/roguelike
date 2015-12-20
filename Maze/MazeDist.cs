085ca1329626dc2f9e0ac73b04164d3f00f3356a
Kazuo Horikawa <horikawa-k@interact-web.co.jp>
2015-12-20 20:49:12 +0900

    Added copyright notice

--- Maze/MazeDist.cs
+++ Maze/MazeDist.cs
@@ -1,4 +1,33 @@
-﻿using System;
+﻿/*
+Copyright(c) 2015, piyonkitch<kazuo.horikawa.ko@gmail.com>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of roguelike nor the names of its
+ contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED.IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +44,7 @@ namespace Maze
         public GridDist[,] grid = new GridDist[Constant.NGRID, Constant.NGRID]; // 迷路のグリッド
         private List<GridDist> candidates;
 
+        // initialize a maze (dungeon) using random number generator.
         public override void initmaze()
         {
             Random rnd = new Random();
@@ -34,7 +64,7 @@ namespace Maze
             grid[0, 0].isWall = false; // スタート地点は壁なしにする
         }
 
-        // 隣り合う Grid を受け付け、fromからtoへは、どの方向に歩けばよいかを返す
+        // returen a direction string to reach "from" to "to".
         private string direction(Grid from, Grid to)
         {
             if (from.xpos - 1 == to.xpos) return "←";
@@ -44,6 +74,7 @@ namespace Maze
             return "?";
         }
 
+        // reverse directions in a string - e.g. For "→↑", "←↓" will be returned.
         private string reverse(string dir)
         {
             if (dir == "←") return "→";
@@ -76,6 +107,8 @@ namespace Maze
             candidates.Add(c); // 末尾に、子供を入れる
         }
 
+        // Walk (fromx, fromy) to (tox, toy) and returns a direction string.
+        // If I cannot reach (tox, toy), then "" will be returned.
         public override string walk(int fromx, int fromy, int tox, int toy)
         {
             for (int y = 0; y < Constant.NGRID; y++)
@@ -124,21 +157,25 @@ namespace Maze
             }
         }
 
-        // 壁
+        // Is (x, y) wall?
         public override bool isWall(int x, int y)
         {
             return grid[x, y].isWall;
         }
 
-        // 見えるかどうか
+        // Is (x, y) visible?
         public override bool isVisible(int x, int y)
         {
             return grid[x, y].isVisible;
         }
+
+        // Make (x, y) visible.
         public override bool Visible(int x, int y)
         {
             return grid[x, y].isVisible = true;
         }
+
+        // Make (x, y) invisible.
         public override bool Invisible(int x, int y)
         {
             return grid[x, y].isVisible = false;
